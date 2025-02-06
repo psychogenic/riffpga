896be0cff17ff3523c89375935315b455b5a9085
Pat Deegan <pat.devel@psychogenic.com>
2025-02-06 15:31:43 -0500

    Better docs for the generic.h config defaults

--- src/config_defaults/generic.h
+++ src/config_defaults/generic.h
@@ -13,6 +13,10 @@
  *   * on burning firmware with a higher revision (subject to
  *     SYSTEM_NEWER_VERSION_AUTO_RESET)
  *
+ * All the pin numbers within are RP2 GPIO numbers, so e.g. setting
+ *  #define PIN_WHATEVER 10
+ * would mean GPIO10 on an RP2040 (physical pin 13 on the QFN)
+ *
  *  Created on: Dec 20, 2024
  *      Author: Pat Deegan
  *    Copyright (C) 2025 Pat Deegan, https://psychogenic.com
@@ -34,36 +38,55 @@
 #ifndef SRC_BOARD_CONFIG_DEFAULTS_GENERIC_H_
 #define SRC_BOARD_CONFIG_DEFAULTS_GENERIC_H_
 
+/*
+ * The name of this board
+ */
 #define BOARD_NAME          "UF2FPGA"
+
+/*
+ * DRIVE_VOLUME_LABEL: what gets mounted as
+ * the drive name
+ */
 #define DRIVE_VOLUME_LABEL  "FPGAUPDATE"
 
 
-// RP2 clock frequency
-// TinyUSB wants it to be 120M but it's
-// worked fine from 100 to 126 so far... still
-#define BOARD_SYSTEM_CLOCK_FREQ_HZ		120000000
+// Remember: RP2 pins -- as GPIO#
 
-
-// RP2 pins -- as GPIO #
+/*
+ * Some LED connected to the RP--this is useful to
+ * provide info about state program slot/unprogrammed.
+ */
 #define PIN_RP_LED			25
 
+/*
+ * Something we use to reset the FPGA.
+ * Should have pull-up/pull-down as appropriate to
+ * allow operation by default
+ */
 #define PIN_FPGA_RESET		4
-// SPI needs to be a valid set of SPI lines
+/*
+ * Assuming Lattice like CRAM that's basically
+ * just SPI, here.
+ * SPI needs to be a valid set of SPI lines
+ * for the RP2.  See 1.4.3. GPIO Functions
+ * in https://datasheets.raspberrypi.com/rp2040/rp2040-datasheet.pdf
+ *
+ * The CS pin, in this case, or whatever the mechanism is
+ * to tell the FPGA that it is a *slave* should have pull-up/pull-down
+ * as appropriate to keep it in this mode by default.
+ */
 #define PIN_FPGA_SPI_MISO	0
 #define PIN_FPGA_SPI_CS		1
 #define PIN_FPGA_SPI_SCK	2
 #define PIN_FPGA_SPI_MOSI	3
-// CDONE-type pin
+// CDONE-type pin, see FPGA_PROG_DONE_LEVEL
 #define PIN_FPGA_PROG_DONE	9
 
-// UART -- rx/tx from RP2's point of view
-#define PIN_UART_BRIDGE_TX	8 /* TODO:FIXME */
-#define PIN_UART_BRIDGE_RX	9 /* TODO:FIXME */
 
 
 // only autoclock1 is currently used
-#define PIN_AUTOCLOCK1		5 /* TODO:FIXME */
-#define PIN_AUTOCLOCK2		6 /* TODO:FIXME */
+#define PIN_AUTOCLOCK1		5 /* the RP2 pin we use for clocking the FPGA  */
+#define PIN_AUTOCLOCK2		6 /* reserved for future, ignored for now */
 
 
 // freq is in Hz
@@ -73,6 +96,12 @@
 #define AUTOCLOCK2_DEFAULT_FREQ		1000000UL
 #define AUTOCLOCK2_DEFAULT_ENABLE	0
 
+// RP2 clock frequency
+// TinyUSB wants it to be 120M but it's
+// worked fine from 100 to 126 so far... still
+#define BOARD_SYSTEM_CLOCK_FREQ_HZ		120000000UL
+
+
 
 /*
  * FPGA_RESET_EXTERNALLY_TRIGGERED
@@ -80,48 +109,34 @@
  * and this may be monitored on PIN_FPGA_RESET
  */
 #define FPGA_RESET_EXTERNALLY_TRIGGERED		0
-#define FPGA_PROG_DONE_LEVEL	1 /* 1==HIGH on PIN_FPGA_PROG_DONE means program success */
-
 #define FPGA_RESET_INVERTED 	1
-#define FLASH_SPI_CS_INVERTED 	1
+
+#define FLASH_SPI_CS_INVERTED 	1 /* 1==CS LOW means selected */
 #define FLASH_SPI_POLARITY		0
 #define FLASH_SPI_PHASE 		0
 #define FLASH_SPI_BAUDRATE 			4000000UL
 
 
-/*
- * UF2 magic numbers and family -- let's us
- * ignore irrelevant data, can be any uint32_t
- *
- * BIN_UF2_* are for the bitstream binary file.
- */
-#define BIN_UF2_MAGIC_START1    0x951C0634UL // Randomly selected
-#define BIN_UF2_MAGIC_END       0x1C73C401UL // Ditto
-#define BIN_UF2_FAMILY_ID		0x6e2e91c1UL
-
+#define FPGA_PROG_DONE_LEVEL	1 /* 1==HIGH on PIN_FPGA_PROG_DONE means program success */
 
 /*
- * The remaining UF2 magiks are used internally, you
- * needn't change them, but may.
+ * UART bridge defaults.
+ * The UART bridge, when enabled, will shuttle all the
+ * data coming in over to the FPGA through UART_BRIDGE_PIN_TX.
+ * Anything coming to it over UART_BRIDGE_PIN_RX gets sent back
+ * over the USB serial connection.
+ * The way to get out of this mode: see UART_BRIDGE_ESCAPE_SEQUENCE*
  */
-// size marker wrapped in UF2
-#define BITSTREAM_UF2_MAGIC_START1		0x951C0512UL
-#define BITSTREAM_UF2_MAGIC_END			0xE4D951C0UL
-#define BITSTREAM_UF2_FAMILY_ID			0x951C0FA3UL
-
-
-
-// board config wrapped in UF2
-#define BOARDCONF_UF2_MAGIC_START1		0x951C057EUL
-#define BOARDCONF_UF2_MAGIC_END			0xC401061EUL
-#define BOARDCONF_UF2_FAMILY_ID			0xB0A2DC0FUL
-
-
 #define UART_BRIDGE_BAUDRATE 	115200
-#define UART_BRIDGE_ENABLED		0
+#define UART_BRIDGE_ENABLED		0 /* probably best to default to 0==off */
+
+// The TX/RX pin, as seen from the RP2's point of view
+#define UART_BRIDGE_PIN_TX	 	8 /* an RP2 pin you can use as a UART TX */
+#define UART_BRIDGE_PIN_RX	 	9 /* an RP2 pin you can use as a UART RX */
+
+// UART_BRIDGE_DEVICEIDX -- the RP uart0/uart1 index we are
+// using.  Set the index according to the selected pins
 #define UART_BRIDGE_DEVICEIDX 	1
-#define UART_BRIDGE_PIN_TX	 	8
-#define UART_BRIDGE_PIN_RX	 	9
 
 /*
  * UART_BRIDGE_ESCAPE_*
@@ -131,8 +146,26 @@
  * unlikely to transmit through the bridge.
  */
 #define UART_BRIDGE_ESCAPE_SEQUENCE0	0x1b
-#define UART_BRIDGE_ESCAPE_SEQUENCE1	0x03
-#define UART_BRIDGE_ESCAPE_SEQUENCE2	0x04
+#define UART_BRIDGE_ESCAPE_SEQUENCE1	0x1b
+#define UART_BRIDGE_ESCAPE_SEQUENCE2	0x1b
+
+
+
+
+/*
+ * UF2 magic numbers and family -- let's us
+ * ignore irrelevant data, can be any uint32_t
+ *
+ * BIN_UF2_* are for the bitstream binary file,
+ * the thing that gets sent over to configure the
+ * FPGA.
+ */
+#define BIN_UF2_MAGIC_START1    0x951C0634UL // Randomly selected
+#define BIN_UF2_MAGIC_END       0x1C73C401UL // Ditto
+#define BIN_UF2_FAMILY_ID		0x6e2e91c1UL
+
+
+
 
 /*
  * We have 4 slots for switches on the PCB, connected
@@ -194,6 +227,26 @@
  */
 #define SYSTEM_INPUTS_NUM					4
 #define SYSTEM_INPUTS_IO_LIST				16,17,18,19
+
+
+
+
+/*
+ * The remaining UF2 magiks are used internally, you
+ * needn't change them, but may if you please.
+ */
+// size marker wrapped in UF2
+#define BITSTREAM_UF2_MAGIC_START1		0x951C0512UL
+#define BITSTREAM_UF2_MAGIC_END			0xE4D951C0UL
+#define BITSTREAM_UF2_FAMILY_ID			0x951C0FA3UL
+
+
+
+// board config wrapped in UF2
+#define BOARDCONF_UF2_MAGIC_START1		0x951C057EUL
+#define BOARDCONF_UF2_MAGIC_END			0xC401061EUL
+#define BOARDCONF_UF2_FAMILY_ID			0xB0A2DC0FUL
+
 
 
 /*
